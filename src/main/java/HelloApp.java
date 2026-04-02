6fe8b17eb0297d5411be912d8ba4bad2f1504a23
aviedu1 <ar7806@srmist.edu.in>
2026-04-02 12:17:02 +0530

    uc4 pushed

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,13 +1,19 @@
 public class HelloApp {
     public static void main(String[] args) {
+
         String name;
 
-        if (args.length > 0) {
-            name = args[0];
-        } else {
+        // If no arguments, use default
+        if (args.length == 0) {
             name = "World";
+        }
+
+        // If arguments exist, join all names
+        else {
+            name = String.join(", ", args);
         }
 
         System.out.println("Hello, " + name + "!");
     }
+    
 }
